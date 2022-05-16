8c79e2c0b0c632333496f91f8436df8e0ff4347e
Ajayraoyadav <97391719+Ajayraoyadav@users.noreply.github.com>
2022-05-16 20:59:19 +0530

    update

---
+++ reactass/src/RCT-101.A1 - React Intro /REACT NAVBAR/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <div>
+        <h1>heloo world</h1>
+    </div>
+  )
+}
---
+++ reactass/src/RCT-101.A1 - React Intro /REACT NAVBAR/navbar.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Logo from "./Logo";
+
+
+
+export default function Navbar() {
+  return (
+    <>
+      <div
+        style={{
+          background: "black",
+          display: "flex",
+          justifyContent: "space-around",
+        }}
+      >
+        <h3 style={{ color: "white" }}>LOGOBAKERY</h3>
+        <div style={{display:"flex"}}>
+          <h3>
+            <a
+              style={{
+                textDecoration: "none",
+                color: "white",
+                marginRight: "10px",
+              }}
+              href={Logo}
+            >
+              Logo
+            </a>
+          </h3>
+          <h3>
+            <a
+              style={{
+                textDecoration: "none",
+                color: "white",
+                marginRight: "10px",
+              }}
+              href="Links"
+            >
+              Links
+            </a>
+          </h3>
+          <h3>
+            {" "}
+            <a
+              style={{
+                textDecoration: "none",
+                color: "white",
+                marginRight: "10px",
+              }}
+              href="Button"
+            >
+              Button
+            </a>
+          </h3>
+        </div>
+        <button
+          style={{
+              marginTop:"10px",
+            borderRadius: "20px",
+            width: "100px",
+            height: "40px",
+            background: "skyblue",
+            color: "white",
+            border: "0",
+            cursor: "pointer",
+          }}
+        >
+          Contact
+        </button>
+      </div>
+    </>
+  );
+}
