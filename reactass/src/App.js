08f1dfb226aa28c2374bf468d966e530c163f61d
Ajayraoyadav <97391719+Ajayraoyadav@users.noreply.github.com>
2022-05-17 18:08:28 +0530

    update

--- reactass/src/App.js
+++ reactass/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Main from "./RCT-101.A1 - React Intro /REACT MOBILE OS/Main"
 import Navbar from './RCT-101.A1 - React Intro /REACT NAVBAR/navbar';
 import Counter from './RCT-101.A2 State, Props And Conditional Rendering/COUNTER/Counter';
+import { TodoList } from './RCT-101.A2 State, Props And Conditional Rendering/REACT SIMPLE TODO/Todolist';
 
 function App() {
   return (
     <div className="App">
       {/* <Main/> */}
       {/* <Navbar/> */}
-      <Counter/>
+      {/* <Counter/> */}
+      <TodoList/>
       
     </div>
   );
