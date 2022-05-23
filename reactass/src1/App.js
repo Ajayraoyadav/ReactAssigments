c6c4b25734aeadd34534d7f970a543367f3b6471
Ajayraoyadav <97391719+Ajayraoyadav@users.noreply.github.com>
2022-05-23 16:23:30 +0530

    update

---
+++ reactass/src1/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from "./RCT-101.A1 - React Intro /REACT MOBILE OS/Main"
+import Navbar from './RCT-101.A1 - React Intro /REACT NAVBAR/navbar';
+import Counter from './RCT-101.A2 State, Props And Conditional Rendering/COUNTER/Counter';
+import { TodoList } from './RCT-101.A2 State, Props And Conditional Rendering/REACT SIMPLE TODO/Todolist';
+import { CartDetails } from './RCT-101.A3 Listing Keys and Conditional Rendering /REACT - PAYMENT CARD/cartdetails';
+import DataDetails from './RCT-101.A3 Listing Keys and Conditional Rendering /RESTAURANT DETAILS/DataDetails';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Main/> */}
+      {/* <Navbar/> */}
+      {/* <Counter/> */}
+      {/* <TodoList/> */}
+      {/* <CartDetails/> */}
+      {/* <DataDetails/> */}
+      
+
+    </div>
+  );
+}
+
+export default App;
